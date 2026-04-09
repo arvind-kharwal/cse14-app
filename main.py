4bb66821bb3716a588efc8438702418d22f94737
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 16:16:25 +0530

    CSE14 9 April 2026

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import streamlit as st
 about_page = st.Page(
     page = 'views/about.py',
     title='About us',
+    icon=':material/home:',
     default=True
 )
 
 contact_page= st.Page(
     page = 'views/contact.py',
     title='Contact us',
+    icon=":material/thumb_up:"
 )
 
 profile_page= st.Page(
     page = 'views/profile.py',
     title='My Profile',
+    icon=":material/person:",
 )
 
 nb = st.navigation(pages=[about_page,profile_page,contact_page])
