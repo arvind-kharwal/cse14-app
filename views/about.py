194a3c22137bb4f848d231887317c35ab9d2a324
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 16:27:00 +0530

    CSE14 9 April 2026

--- views/about.py
+++ views/about.py
@@ -2,4 +2,3 @@ import streamlit as st
 st.title('About us')
 st.balloons()
 st.divider()
-st.snow()
--- views/contact.py
+++ views/contact.py
@@ -1,2 +1,3 @@
 import streamlit as st
 st.title('Contact Us')
+st.info('This is a purely informational message', icon="ℹ️")
